4fe7073a5c34bcf2cfe983cac32cff22b963f41d
Sapir Shemer <lesscomplexity@gmail.com>
2020-10-29 14:46:41 +0200

    Functions, Primitives, arrays and loops part 1

---
+++ FunctionsAndPrimitives/FunctionsAndPrimitives/FunctionsAndPrimitives.cpp
@@ -0,0 +1,24 @@
+﻿// FunctionsAndPrimitives.cpp : Defines the entry point for the application.
+//
+
+#include "FunctionsAndPrimitives.h"
+
+using namespace std;
+
+int multiply(int a, int b) {
+	cout << a << ", " << b << endl;
+	return a * b;
+}
+
+int main()
+{
+	int a = multiply(10, 20);
+	cout << a << endl;
+	cout << multiply(20, 10) << endl;
+
+	if (a != 5) {
+		int b = multiply(40, 12);
+		cout << "Haha" << endl;
+		cout << b << endl;
+	}
+}
--- Noam First/NoamFirst/NoamFirst/NoamFirst.cpp
+++ Noam First/NoamFirst/NoamFirst/NoamFirst.cpp
@@ -5,22 +5,42 @@
 
 using namespace std;
 
+void ignore_cin() {
+	char a = cin.get();
+	while (a != '\n') {
+		cout << "Ignoring: " << a << endl;
+		a = cin.get();
+	}
+	cout << "A new line is found, the input has been cleaned." << endl;
+}
+
+/**
+ * The same as ignore_cin() but faster and without output
+ */
+void super_ignore_cin() {
+	while (cin.get() != '\n');
+}
+
 int main()
 {
 	int childrens;
 	int brothers;
 	char response;
-	cout << "How many childrens do you wish to have?" << endl;
+	cout << "How many childrens do you wish to have?\n";
 	cin >> childrens;
 	cout << "And how many brothers do you have?" << endl;
 	cin >> brothers;
-	cout << "so you have " << brothers; cout << " brothers and you want " << childrens; cout << " childrens?" << endl;
-	cin >> response;
+	cout << "so you have " << brothers;
+	cout << " brothers and you want " << childrens; cout << " childrens?" << endl;
+	cin >> response; // Get n
+	cout << "The input is: " << response << endl;
+	ignore_cin();
 
 	while (response != 'y')
 	{
 		cout << "Come on man press the y already" << endl;
 		cin >> response;
+		ignore_cin();
 	}
 
 	if (response == 'y')
