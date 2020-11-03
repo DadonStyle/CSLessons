e19915fdd4392f7896c23c2e43d79f9f6af645e4
Sapir Shemer <lesscomplexity@gmail.com>
2020-11-03 17:46:43 +0200

    Lesson 4: For loops & Introduction to abstract thinking

--- ArraysLessonsTask02.11.20/ArraysLessonsTask02.11.20/ArraysLessonsTask02.11.20.cpp
+++ ArraysLessonsTask02.11.20/ArraysLessonsTask02.11.20/ArraysLessonsTask02.11.20.cpp
@@ -1,45 +1,64 @@
 ﻿// ArraysLessonsTask02.11.20.cpp : Defines the entry point for the application.
 //
 //Task: take 5 numbers in the form of arrays from the user
-// ask the use if he want to add those numbers or multiplay
+// ask the use if he want to add those numbers or multiply
 // show the result to the use
 #include "ArraysLessonsTask02.11.20.h"
 
 using namespace std;
 
-
-
-
-
 int main()
 {
-	int arr[5];
+	const int size = 10;
+	int arr[size]; // arr[0] <=> arr[size - 1]
 	char response;
+	// int index = 0;
 
-	cout << "Insert 5 numbers" << endl;
-	cin >> arr[0];
-	cout << "number 1 is: " << arr[0] << endl;
-	cin >> arr[1];
-	cout << "number 2 is: " << arr[1] << endl;
-	cin >> arr[2];
-	cout << "number 3 is: " << arr[2] << endl;
-	cin >> arr[3];
-	cout << "number 4 is: " << arr[3] << endl;
-	cin >> arr[4];
-	cout << "number 5 is: " << arr[4] << endl;
+	cout << "Insert " << size << " numbers" << endl;
+	/*
+	while (index < size) {
+		cout << "Enter number " << (index + 1) << ": ";
+		cin >> arr[index];
+		index++; // Move forward in order
+	}
+	*/
 
-	int multiplay(arr[0] * arr[1] * arr[2] * arr[3] * arr[4]);
-	int add(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+	for (int i = 0; i < size; i++) {
+		cout << "Enter number " << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 
-	cout << "would you like to add or multiplay those numbers? Answer + or *" << endl;
+	int multiply = arr[0];
+	int add = arr[0];
+
+	for (int i = 1; i < size; i++) {
+		multiply = multiply * arr[i];
+		add = add + arr[i];
+	}
+
+	// index = 1; // Restart the index
+	/*
+	while (index < size) {
+		multiply = multiply * arr[index];
+		add = add + arr[index];
+		index++; // Important -> If index not changing a pitfall of an infinite loop might be present
+	}
+	*/
+	/*
+	multiply = multiply * arr[2];
+	multiply = multiply * arr[3];
+	multiply = multiply * arr[4];
+	*/
+
+	cout << "would you like to add or multiply those numbers? Answer + or *" << endl;
 	cin >> response;
 
 	switch (response)
 	{
-	case '*': multiplay;
-		cout << "the result is: " << multiplay << endl;
+	case '*':
+		cout << "the result is: " << multiply << endl;
 		break;
-	case '+': add;
+	case '+':
 		cout << "thre result is: " << add << endl;
 		break;
 	}
---
+++ SwapVariables/SwapVariables/SwapVariables.cpp
@@ -0,0 +1,26 @@
+﻿// SwapVariables.cpp : Defines the entry point for the application.
+//
+
+#include "SwapVariables.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	
+	cout << "Enter number a: ";
+	cin >> a;
+	cout << "Enter number b: ";
+	cin >> b;
+
+	cout << "a: " << a << endl << "b: " << b << endl;
+
+	// Swap between a and b
+	c = a;
+	a = b;
+	b = c;
+
+	cout << "a: " << a << endl << "b: " << b << endl;
+	return 0;
+}
