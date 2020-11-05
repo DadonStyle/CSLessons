d5762e05941132ebc2248cca244d926c1b6f1b0b
Sapir Shemer <lesscomplexity@gmail.com>
2020-11-05 11:36:18 +0200

    Lesson 5 Sorting

---
+++ SelectionSort/SelectionSort/SelectionSort.cpp
@@ -0,0 +1,51 @@
+﻿// SelectionSort.cpp : Defines the entry point for the application.
+//
+
+#include "SelectionSort.h"
+
+using namespace std;
+
+int main()
+{
+	const int size = 10;
+	int arr[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Enter number " << i << ": ";
+		cin >> arr[i];
+	}
+
+	int start = 0;
+	int end = size - 1;
+	int max_index = 0;
+	int min_index = 0;
+
+	while (start >= end) {
+		// Reset min and max
+		max_index = start;
+		min_index = start;
+
+		// Find max & min
+		for (int i = start + 1; i <= end; i++) {
+			if (arr[max_index] < arr[i]) max_index = i;
+			if (arr[min_index] > arr[i]) min_index = i;
+		}
+
+		// Set max at end & min at start
+		int temp = arr[end];
+		arr[end] = arr[max_index];
+		arr[max_index] = temp;
+
+		temp = arr[start];
+		arr[start] = arr[min_index];
+		arr[min_index] = temp;
+
+		// Move end backwards & start forwards
+		start++;
+		end--;
+	}
+
+	for (int i = 0; i < size; i++) cout << arr[i] << " ";
+
+	return 0;
+}
--- Task 03.11.20/Task 03.11.20/Task 03.11.20.cpp
+++ Task 03.11.20/Task 03.11.20/Task 03.11.20.cpp
@@ -7,31 +7,23 @@ using namespace std;
 // task:Take the biggest 2 numbers of any size (this case 5 numbers) that the user will insert and multyplay the biggest numbers
 int main()
 {
-	const int size = 4;
+	const int size = 5;
 	int arr[size];
 
     for (int i = 0; i < size; i++) {
-	cout << "Enther the num: " << i + 1 << endl;
-	cin >> arr[i];
- }
-
-	if (arr[0] <= arr[1])
-	{
-		cout << arr[1] << endl;
-		return 0;
+		cout << "Enther the num" << i + 1 << ": " << endl;
+		cin >> arr[i];
 	}
-	else (arr[1] < arr[0]);
-		{ cout << arr[0] << endl; }
-    
 
-		if (arr[2] <= arr[3])
-		{
-			cout << arr[3] << endl;
-			return 0;
-		}
+	int max = arr[0];
+	int min = arr[0];
 
-		else (arr[3] < arr[2]);
-		{ cout << arr[2] << endl; }
-		
+	for (int i = 1; i < size; i++) {
+		// Inline IF -> If an "if", "else", "while" or "for" contains only one line of code, then no scope ({}) is needed
+		if (max < arr[i]) max = arr[i];
+		if (min > arr[i]) min = arr[i]; // Inline IF
+	}
 
+	cout << "Max number entered: " << max << endl;
+	cout << "Min number entered: " << min << endl;
 }
