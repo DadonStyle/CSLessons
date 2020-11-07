21d65380fd94a206bdb377a1c3b54584a38a9fec
DadonStyle <noamoni9@gmail.com>
2020-11-07 20:09:29 +0200

    BubbleSort

---
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,31 @@
+﻿// BubbleSort.cpp : Defines the entry point for the application.
+//
+
+#include "BubbleSort.h"
+
+using namespace std;
+
+int main()
+{
+	const int size = 4;
+	int arr[size];
+	int add = 0;
+	for (int i = 0; i < size; i++) {
+		cout << "Please enter number " << i+1 << ":";
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (arr[j] < arr[j + 1]) {
+				int temp = 0;
+				temp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < size; i++) { cout << arr[i] << " "; }
+
+}
---
+++ Lesson 6 task 05.11.20/Lesson 6 task 05.11.20/Lesson 6 task 05.11.20.cpp
@@ -0,0 +1,41 @@
+﻿// Lesson 6 task 05.11.20.cpp : Defines the entry point for the application.
+//
+
+#include "Lesson 6 task 05.11.20.h"
+// TODO: build a program that asks him: 1. how old is he? 2. what year did he born? 3. and ask for 3 more numbers {do it dinamic} 
+// and than print the sum of all the numbers with the values that he insert
+using namespace std;
+
+
+int main()
+{
+	int age;
+	int YOB;
+	const int size = 4;
+	int arr[size];
+
+	cout << "How old are you?" << endl;
+	cin >> age;
+
+	cout << "What's your year of birth?" << endl;
+	cin >> YOB;
+
+	for (int i = 0; i < size; i++) {
+		cout << "enter number " << i + 1 << ":";
+		cin >> arr[i];
+
+	}
+	
+	int add = arr[0];
+	int	index = 1;
+	
+	while (index < size) {
+		add = add + arr[index];
+		index++;
+	}
+
+	cout << add + age + YOB;
+	
+
+	
+}
