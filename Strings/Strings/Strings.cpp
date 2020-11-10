7e5e3bea3cf278fbfee3140c2ee1e8cd69d0a96c
DadonStyle <noamoni9@gmail.com>
2020-11-10 16:31:35 +0200

    today 10.11.20

--- Strings/Strings/Strings.cpp
+++ Strings/Strings/Strings.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+
+
 int cinString(char * str, int size) { // camelCase
 	int new_size = 0;
 	for (int i = 0; i < size; i++) {
@@ -16,35 +18,60 @@ int cinString(char * str, int size) { // camelCase
 	return new_size;
 }
 
-int main()
-{
-	const int SIZE = 256;
-	char str[SIZE] = { 0 };
-	char find[SIZE] = { 0 };
-	int size_str = 0;
-	int size_find = 0;
+void StringFinder(int size_str, int size_find, char* str, char* find) {
 
-	cout << "Enter A sentence: " << endl;
-	size_str = cinString(str, SIZE);
-
-	cout << "What to find in the sentence: " << endl;
-	size_find = cinString(find, SIZE);
+	
 
 	for (int i = 0; i <= size_str - size_find; i++) {
+		
 		bool is_true = true;
+
 		for (int j = 0; j < size_find; j++) {
+
 			if (str[i + j] != find[j]) {
 				is_true = false;
 				break;
+				
 			}
+
+			
 		}
 
 		if (is_true) {
 			cout << "The position is: " << i << endl;
 			break;
+
 		}
+
 	}
 
-	// TODO: return the start index of the first occurrence of find in str
+	
+	if (str[size_str - size_find] != find[0]) cout << "-1" << endl;
+	
+	
+}
+
+int main()
+{
+
+	const int SIZE = 256;
+	char str[SIZE] = { 0 };
+	char find[SIZE] = { 0 };
+	int size_str = 0;
+	int size_find = 0;
+	
+
+	cout << "Enter A sentence: " << endl;
+	size_str = cinString(str, SIZE);
+	cout << "What to find in the sentence: " << endl;
+	size_find = cinString(find, SIZE);
+
+	StringFinder(size_str, size_find, str, find);
+
+	
+		
+
+
+	// TODO: return the start index of the first occurrence of find in str;
 	return 0;
 }
