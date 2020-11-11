aa3b2018a5ed13bf8797bfe30c3c102f049bfbf2
Sapir Shemer <lesscomplexity@gmail.com>
2020-11-11 17:59:11 +0200

    Practical Lesson 11

--- Primes/Primes/Primes.cpp
+++ Primes/Primes/Primes.cpp
@@ -2,26 +2,36 @@
 //
 
 #include "Primes.h"
+#include <math.h>
 
 using namespace std;
 
 bool isPrime(int n) {
-	for (int i = 2; i < n; i++) { // Ordinal Problem
+	for (int i = 2; i <= sqrt(n); i++) { // Ordinal Problem
 		if (n % i == 0) return false; // Condition
 	}
-
 	return true;
+}
+
+int findPrimeAt(int prime_index) {
+	int counter = 1;
+	int current_prime = 2;
+
+	while (counter < prime_index) {
+		int prime_candidate = current_prime + 1;
+		while (!isPrime(prime_candidate)) prime_candidate++;
+		current_prime = prime_candidate;
+		counter++;
+	}
+
+	return current_prime;
 }
 
 int main()
 {
-
 	int u = 0;
 	cout << "enter a number ";
 	cin >> u;
 
-	for (int i = 0; i < u; i++) {
-		if (isPrime)
-		}
-	}
+	cout << "The prime at " << u << " is: " << findPrimeAt(u);
 }
