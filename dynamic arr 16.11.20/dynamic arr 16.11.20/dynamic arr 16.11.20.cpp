17d3443520e57ab0527237596a5af656db93a4b7
DadonStyle <noamoni9@gmail.com>
2020-11-16 16:05:22 +0200

    new 16.11.20

---
+++ dynamic arr 16.11.20/dynamic arr 16.11.20/dynamic arr 16.11.20.cpp
@@ -0,0 +1,29 @@
+﻿// dynamic arr 16.11.20.cpp : Defines the entry point for the application.
+//
+
+#include "dynamic arr 16.11.20.h"
+
+using namespace std;
+
+int cinString(int* str, int size) { // camelCase
+	int new_size = 0;
+	for (int i = 0; i < size; i++) {
+		char temp = cin.get();
+		if (temp == '0') break;
+		str[i] = temp;
+		new_size++;
+	}
+	return new_size;
+}
+
+int main()
+{
+	const int size = 256;
+	int arr[size];
+
+	cinString(arr, size);
+
+	
+
+
+}
