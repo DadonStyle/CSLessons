994f8db986d16261119635ea3c732677089bbc0d
DadonStyle <noamoni9@gmail.com>
2020-11-25 17:34:52 +0200

    homework

---
+++ BucketSort Function/BucketSort Function/BucketSort Function.cpp
@@ -0,0 +1,41 @@
+﻿// BucketSort Function.cpp : Defines the entry point for the application.
+//
+
+#include "BucketSort Function.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void BucketSort(char arr[], int n) {
+	//create an empty bucket
+	vector<float> b[n];
+
+	//put array elements in different buckets
+		for (int i = 0; i < n; i++) {
+			int bi = n * arr[i]; //index in bucket
+			b[bi].pushback(arr[i]);
+		}
+
+	//sort individual buckets
+		for (int i = 0; i < n; i++) {
+			sort(b[i].begin(),b[i].end());
+		}
+
+	//concatenate all buckets into arr[]
+
+
+}
+
+int main()
+{
+	char arr[] = { 'a', 'b' , 'c' , 'd' , 'e' , 'f' , 'g' };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	BucketSort(arr, n);
+
+	cout << "Sorted array is: ";
+	for (int i = 0; i < n;i++) {
+		cout << arr[i] << " ";
+	}
+
+}
---
+++ HomeWork 24.11.20/HomeWork 24.11.20/HomeWork 24.11.20.cpp
@@ -0,0 +1,37 @@
+﻿// HomeWork 24.11.20.cpp : Defines the entry point for the application.
+//
+
+
+#include "HomeWork 24.11.20.h"
+using namespace std;
+
+#define WIDTH 26
+#define HEIGHT 3
+
+int main()
+{
+
+    char input = ' ';
+	bool currentPlayer = true;
+    char Arena[HEIGHT][WIDTH];
+    char n, m;
+
+    for (n = 0; n < HEIGHT; n++) {
+        for (m = 0; m < WIDTH; m++)
+        {
+            Arena[n][m] = m + 97;
+        }
+    }
+
+    while (input != Arena[1][25]) {
+        cout << "Player " << (currentPlayer ? 1 : 2) << " Turn: ";
+        input = cin.get();
+        cin.get() != '/n';
+        currentPlayer = !currentPlayer;
+    }
+
+    
+
+    
+    
+}
