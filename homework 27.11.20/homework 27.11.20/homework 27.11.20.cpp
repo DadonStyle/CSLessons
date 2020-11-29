a4ba38109c5975ac0c93927d73a6e077d9ffd3dc
DadonStyle <noamoni9@gmail.com>
2020-11-29 17:01:44 +0200

    homework 27/11/20

--- homework 27.11.20/homework 27.11.20/homework 27.11.20.cpp
+++ homework 27.11.20/homework 27.11.20/homework 27.11.20.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "homework 27.11.20.h"
+using namespace std;
 
 int *AddToArray(int* arr, long long int size, int input) {
 	int* temp = new int[size + 1];
@@ -17,71 +18,60 @@ int* concatenate(int* arr1, int* arr2, long long int arr1_size, long long int ar
 	for (int i = 0; i < arr1_size + arr2_size;i++) temp[arr1_size + i] = arr2[i];
 	return temp;
 }
+  
+void DupliKiller(int* arr, int size) {
+	for (int i = 0; i < size;i++) {
+		for (int j = 0; j < size; j++) {
+			if (j + i >= size) j == 0;
 
-                         
- void BubbleSort(int* arr, int size) {						
-	 int temp = 0;
-	for (int i = 0; i < size; i++) {						
-		for (int j = 0; j < size - 1; j++) {					
-			if (arr[j] < arr[j + 1]) {	
-				temp = arr[j];									
-				arr[j] = arr[j+1];							
-				arr[j+1] = temp;				
-			}													
-		}														
-	}															
-}																
+			else if (j == 0 && i == 0) j == 1;
 
-using namespace std;
-//TODO: ask for 2 arrays from the use, concate them, and ignore duplicate numbers
-int main()
+			else if (arr[j] == arr[i]) {
+				size = size - 1;
+				
+			}
 
-{
-	int input = -1;
-	int* arr1 = nullptr;
-	long long int arr1_size = 0;
-	int* arr2 = nullptr;
-	long long int arr2_size = 0;
-	int arr3_size = 0;
-
-	cout << "Please enter the first array: ";
-	while (input != 0) {
-		cin >> input;
-	if (input == 0) break;
-	arr1 = AddToArray(arr1, arr1_size, input);
-	arr1_size++;
-	}
-	
-	cout << "Please enter the second array: ";
-	input = cin.get();
-	while (input != 0) {
-		cin >> input;
-		if (input == 0) break;
-		arr2 = AddToArray(arr2, arr2_size, input);
-		arr2_size++;
-	}
-	
-	int * arr3 = concatenate(arr1, arr2, arr1_size, arr2_size);
-	for (int i = 0; i < arr1_size + arr2_size;i++) cout << "|" << arr3[i];
-	cout << endl;
-	arr3_size = arr1_size + arr2_size;
-
-	BubbleSort(arr3, arr3_size);
-
-	for (int i = 0; i < arr3_size - 1;i++) {
-		if (arr3[i] != arr3[i + 1]) {
-			cout << "|" << arr3[i];
+			else continue;
+			
 		}
-		else continue;
 	}
-		
-
-
-	
-
-	
-
-
-	
-
 }
+
+ int main()
+
+ {
+	 int input = -1;
+	 int* arr1 = nullptr;
+	 long long int arr1_size = 0;
+	 int* arr2 = nullptr;
+	 long long int arr2_size = 0;
+	 int arr3_size = 0;
+
+	 cout << "Please enter the first array: ";
+	 while (input != 0) {
+		 cin >> input;
+		 if (input == 0) break;
+		 arr1 = AddToArray(arr1, arr1_size, input);
+		 arr1_size++;
+	 }
+
+	 cout << "Please enter the second array: ";
+	 input = cin.get();
+	 while (input != 0) {
+		 cin >> input;
+		 if (input == 0) break;
+		 arr2 = AddToArray(arr2, arr2_size, input);
+		 arr2_size++;
+	 }
+
+	 int* arr3 = concatenate(arr1, arr2, arr1_size, arr2_size);
+	 for (int i = 0; i < arr1_size + arr2_size;i++) cout << "|" << arr3[i];
+	 cout << endl;
+	 arr3_size = arr1_size + arr2_size;
+
+	 DupliKiller(arr3, arr3_size);
+
+	 for (int i = 0; i < arr3_size;i++) cout << "|" << arr3[i];
+
+	 
+ }
